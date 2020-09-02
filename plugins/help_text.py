5795909877daba57706af10bd3bb544303df50bc
Ns-AnoNymouS <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-09-02 12:08:29 +0530

    Update help_text.py

--- plugins/help_text.py
+++ plugins/help_text.py
@@ -37,7 +37,7 @@ async def help_user(bot, update):
     TRChatBase(update.from_user.id, update.text, "/help")
     await bot.send_message(
         chat_id=update.chat.id,
-        text=Translation.HELP_USER.format(update.from_user.first_name),
+        text=Translation.HELP_USER.format(update.from_user.first_name, config.USER_NAME),
         parse_mode="html",
         disable_web_page_preview=True,
         reply_to_message_id=update.message_id
