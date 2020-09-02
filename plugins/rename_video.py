6d93e8b99fb36afb73dd521d6ca852ed6e94c472
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-09-02 17:04:01 +0530

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -45,10 +45,10 @@ async def rename_video(bot, update):
     TRChatBase(update.from_user.id, update.text, "rename")
     if (" " in update.text) and (update.reply_to_message is not None):
         cmd, file_name = update.text.split(" ", 1)
-        if len(file_name) > 6400:
+        if len(file_name) > 64:
             await update.reply_text(
                 Translation.IFLONG_FILE_NAME.format(
-                    alimit="6400",
+                    alimit="64",
                     num=len(file_name)
                 )
             )
