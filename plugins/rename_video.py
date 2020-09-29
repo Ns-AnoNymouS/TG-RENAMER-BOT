08bb672498063700852334bcbaf2d9a131eb2f21
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-09-29 12:05:03 +0530

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -10,7 +10,7 @@ logger = logging.getLogger(__name__)
 
 import os
 import time
-
+import random
 # the secret configuration specific things
 if bool(os.environ.get("WEBHOOK", False)):
     from sample_config import Config
