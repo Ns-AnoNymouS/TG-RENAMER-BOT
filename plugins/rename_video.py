03ca576fab7281e6f2381e12a8cbc66bd8e49e20
Anonymous <70622189+Ns-AnoNymouS@users.noreply.github.com>
2020-10-11 11:19:00 +0530

    Update rename_video.py

--- plugins/rename_video.py
+++ plugins/rename_video.py
@@ -92,11 +92,17 @@ async def rename_video(bot, update):
             height = 0
             duration = 0
             metadata = extractMetadata(createParser(new_file_name))
-            if metadata.has("duration"):
+            try:
+             if metadata.has("duration"):
                 duration = metadata.get('duration').seconds
+            except:
+              pass
             thumb_image_path = Config.DOWNLOAD_LOCATION + "/" + str(update.from_user.id) + ".jpg"
             if not os.path.exists(thumb_image_path):
+               try:
                     thumb_image_path = await take_screen_shot(new_file_name, os.path.dirname(new_file_name), random.randint(0, duration - 1))
+               except:
+                    thumb_image_path = None
             else:
                 width = 0
                 height = 0
